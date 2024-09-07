a497bb961764d87d2d79d92bda2ebe112ceb0152
Peter Lonergan <lonpete@outlook.com>
2024-09-07 10:40:26 +1000

    Tightly coupled classes, but it works.

--- src/main/java/document/Document.java
+++ src/main/java/document/Document.java
@@ -1,9 +1,14 @@
 package document;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Document {
     private String text = "";
     private final double margin;
     private final String author;
+    private Deque<DocumentMemento> undoStack = new ArrayDeque<>();
+    private Deque<DocumentMemento> redoStack = new ArrayDeque<>();
 
     public Document(double margin, String author) {
         this.margin = margin;
@@ -16,14 +21,30 @@ public class Document {
     }
 
     public void append(String text) {
+        undoStack.push(createMemento());
         this.text += text + "\n";
+        redoStack.clear();
     }
 
-    public DocumentMemento createMemento() {
+    public void undo() {
+        if (!undoStack.isEmpty()) {
+            redoStack.push(createMemento());
+            restoreFromMemento(undoStack.pop());
+        }
+    }
+
+    public void redo() {
+        if (!redoStack.isEmpty()) {
+            undoStack.push(createMemento());
+            restoreFromMemento(redoStack.pop());
+        }
+    }
+
+    private DocumentMemento createMemento() {
         return new DocumentMemento(this.text);
     }
 
-    public void restoreFromMemento(DocumentMemento memento) {
+    private void restoreFromMemento(DocumentMemento memento) {
         this.text = memento.getText();
     }
 }
--- src/main/java/document/DocumentSystem.java
+++ src/main/java/document/DocumentSystem.java
@@ -3,72 +3,65 @@ package document;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class DocumentSystem {
-    private static final String FOLDER_NAME = "output";
-    private Deque<DocumentMemento> undoStack = new ArrayDeque<>();
-    private Deque<DocumentMemento> redoStack = new ArrayDeque<>();
 
-    public Document createDocument() {
+    // DO NOT MODIFY
+    private static final String FOLDER_NAME = "output";
+
+    public static Document createDocument() {
         return new Document(12, "Pete");
     }
 
-    public void append(Document document, String text) {
-        undoStack.push(document.createMemento());
+    public static void append(Document document, String text) {
         document.append(text);
-        redoStack.clear();
     }
 
-    public void undo(Document document) {
-        if (!undoStack.isEmpty()) {
-            redoStack.push(document.createMemento());
-            document.restoreFromMemento(undoStack.pop());
-        }
+    public static void undo(Document document) {
+        document.undo();
     }
 
-    public void redo(Document document) {
-        if (!redoStack.isEmpty()) {
-            undoStack.push(document.createMemento());
-            document.restoreFromMemento(redoStack.pop());
-        }
+    public static void redo(Document document) {
+        document.redo();
     }
 
     public static void main(String[] args) {
+        // DO NOT MODIFY
         test();
     }
 
     public static void test() {
-        DocumentSystem system = new DocumentSystem();
-        Document document = system.createDocument();
+        // DO NOT MODIFY
+        Document document = createDocument();
         File folder = new File(FOLDER_NAME);
         folder.mkdir();
 
         int i = 0;
+
         while (i < 3) {
-            system.append(document, String.format("Line %d", i));
+            append(document, String.format("Line %d", i));
             i += 1;
         }
 
-        writeDocumentToFile(document, "original.txt");
+        writeDocumentToFile(document, String.format("original.txt", i));
 
-        system.undo(document);
+        undo(document);
         writeDocumentToFile(document, "1-undo.txt");
-        system.undo(document);
+        undo(document);
         writeDocumentToFile(document, "2-undo.txt");
 
-        system.redo(document);
+        redo(document);
         writeDocumentToFile(document, "3-redo.txt");
-        system.redo(document);
+        redo(document);
         writeDocumentToFile(document, "4-redo.txt");
     }
 
-    private static void writeDocumentToFile(Document document, String filename) {
+    public static void writeDocumentToFile(Document document, String filename) {
+        // DO NOT MODIFY
         try (FileWriter writer = new FileWriter(FOLDER_NAME + "/" + filename)) {
             writer.write(document.toString());
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Handle the exception
         }
     }
 }
